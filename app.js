cb7dc06990fea57553a4845f340bcdf079c7f590
Manuel Duran <manuelmid99@gmail.com>
2021-10-24 14:09:57 +0200

    working on the styles / js functions

---
+++ app.js
@@ -0,0 +1,9 @@
+var button1 = document.getElementById("warm");
+var button2 = document.getElementById("cold");
+
+console.log(button1);
+console.log(button2);
+
+button1.addEventListener('click', function(){
+    alert('Clicked');
+});
