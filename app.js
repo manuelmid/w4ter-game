91a7e7edeaa17968b5e3b8b21e5d65aae7f9d6c1
Manuel Duran <manuelmid99@gmail.com>
2021-11-26 15:54:47 +0100

    working on the js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var button1 = document.getElementById("warm");
 var button2 = document.getElementById("cold");
 
+var rotorOne = document.getElementById("rotor-1");
+var rotorTwon = document.getElementById("rotor-2");
+
 console.log(button1);
 console.log(button2);
 
 button1.addEventListener('click', function(){
-    alert('Clicked');
+    rotorOne.style.animation = "rotar 1s linear infinite";
+});
+
+button2.addEventListener('click', function(){
+    rotorTwon.style.transform = "rotateZ(-90deg)";
 });
