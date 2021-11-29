a4f5c1cedd204a409045d84819edd575bd943106
Manuel Duran <manuelmid99@gmail.com>
2021-11-29 23:22:13 +0100

    creating a press and hold function for the buttons

--- app.js
+++ app.js
@@ -1,8 +1,77 @@
-var button1 = document.getElementById("warm");
+var item = document.getElementById("warm");
 var button2 = document.getElementById("cold");
 
 var rotorOne = document.getElementById("rotor-1");
 var rotorTwon = document.getElementById("rotor-2");
+
+
+// let item = document.querySelector("#item");
+
+    let timerID;
+    let counter = 0;
+
+    let pressHoldEvent = new CustomEvent("pressHold");
+
+    // Increase or decreae value to adjust how long
+    // one should keep pressing down before the pressHold
+    // event fires
+    let pressHoldDuration = 50;
+
+    // Listening for the mouse and touch events    
+    item.addEventListener("mousedown", pressingDown, false);
+    item.addEventListener("mouseup", notPressingDown, false);
+    item.addEventListener("mouseleave", notPressingDown, false);
+
+    item.addEventListener("touchstart", pressingDown, false);
+    item.addEventListener("touchend", notPressingDown, false);
+
+    // Listening for our custom pressHold event
+    item.addEventListener("pressHold", doSomething, false);
+
+    function pressingDown(e) {
+      // Start the timer
+      requestAnimationFrame(timer);
+
+      e.preventDefault();
+
+      console.log("Pressing!");
+    }
+
+    function notPressingDown(e) {
+      // Stop the timer
+      cancelAnimationFrame(timerID);
+      counter = 0;
+
+      console.log("Not pressing!");
+    }
+
+    //
+    // Runs at 60fps when you are pressing down
+    //
+    function timer() {
+      console.log("Timer tick!");
+
+      if (counter < pressHoldDuration) {
+        timerID = requestAnimationFrame(timer);
+        counter++;
+      } else {
+        console.log("Press threshold reached!");
+        item.dispatchEvent(pressHoldEvent);
+      }
+    }
+
+    function doSomething(e) {
+      console.log("pressHold event fired!");
+    }
+
+
+
+
+
+
+
+
+
 
 console.log(button1);
 console.log(button2);
@@ -12,5 +81,5 @@ button1.addEventListener('click', function(){
 });
 
 button2.addEventListener('click', function(){
-    rotorTwon.style.animation = "rotarR 1s linear";
+    rotorTwon.style.transform = "rotateZ(-90deg)";
 });
